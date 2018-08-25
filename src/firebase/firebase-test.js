6448ecf6f91bf6f4aaa216a481a9d2b180d775f3
Andres <andresrod21@gmail.com>
2018-08-25 14:29:49 -0400

    API test tweaking

--- src/firebase/firebase-test.js
+++ src/firebase/firebase-test.js
@@ -1,15 +1,31 @@
 const firebase = require('firebase');
 
-require('dotenv').config({ path: '.env.test' });
+process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
-const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
+let config;
+
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test' });
+    config = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    };
+} else {
+    config = {
+        apiKey: process.env.TEST_FIREBASE_API_KEY,
+        authDomain: process.env.TEST_FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.TEST_FIREBASE_DATABASE_URL,
+        projectId: process.env.TEST_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.TEST_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.TEST_FIREBASE_MESSAGING_SENDER_ID
+    };
+}
+
+
 
 firebase.initializeApp(config);
 
